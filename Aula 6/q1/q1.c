bd1293806d4d8c29590bfdb4e113b20534b3c99a
Thiago Rocha <81600149+thiagorochatr@users.noreply.github.com>
2022-07-13 20:12:09 -0300

    Add files via upload

---
+++ Aula 6/q1/q1.c
@@ -0,0 +1,45 @@
+#include <conio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+void leArranjo(int a[], int t) {
+    int i;
+    for (i = 0; i < t; i++) {
+        printf("Digite o valor da %d posicao: ", i+1);
+        scanf("%d", &a[i]);
+    }
+}
+
+void trocaArray(int *a[], int t) {
+    int aux, i;
+    int j = t-1;
+    if(t % 2 == 0) {
+        for (i = 0; i < t/2; i++) {
+            aux = a[i];
+            a[i] = a[j];
+            a[j] = aux;
+            j--;
+        }
+    } else {
+            for (i = 0; i <= t/2; i++) {
+            aux = a[i];
+            a[i] = a[j];
+            a[j] = aux;
+            j--;
+        }
+    }
+}
+
+main() {
+    int t;
+    printf("Digite o tamanho da Array: ");
+    scanf("%d", &t);
+    int a[t];
+    int i;
+  leArranjo(a, t);
+  trocaArray(&a, t);
+  for (i = 0; i < t; i++) {
+    printf("%d", a[i]);
+  }
+}
